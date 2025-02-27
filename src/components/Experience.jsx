9ea60042f3ba236fe649acb7c307f72e50f935b6
noveon-cguillermo <cguillermo@noveon.co>
2025-02-27 03:08:53 -0600

    added hero section and chose some colors for the background and the experience tiles

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,5 +1,16 @@
 import { useState } from 'react'
-import { motion } from 'framer-motion';
+import { easeIn, easeInOut, motion } from 'framer-motion';
+
+const titleVariant={
+  visible: {
+    x: ['100%', 0],
+    transition: {
+      duration: 0.5,
+      ease: easeInOut,
+      type: 'spring',
+    }
+  }
+}
 
 function Experience() {
 
@@ -20,13 +31,19 @@ function Experience() {
   return (
     <>
     <motion.div className='exp'>
-      <h2 className='text-4xl'>Experience</h2>
-      <hr />
-      <div className="grid grid-cols-3 gap-4 p-3">
-        <motion.div className="bg-blue-500 p-4 flip-card rounded-lg cursor-pointer"
-          onClick={() => handleFlip(0)}
+      <motion.h2 className='p-4 text-4xl'
+        variants={titleVariant}
+        whileInView='visible'
+        viewport={{ once: true }}
+      >Experience</motion.h2>
+
+      <hr className='p-4' />
+      <div className="grid grid-cols-3 gap-4 p-2">
+      <motion.div className="p-4 flip-card rounded-lg cursor-pointer"
+          onClick={() => handleFlip(2)}
           initial = {{opacity: 0, y: '20vh'}}
-          animate = {{opacity: 1, y: 0, rotateY: flipped[0] ? 180: 0}}
+          whileInView = {{opacity: 1, y: 0, rotateY: flipped[2] ? 180: 0}}
+          viewport={{ once: true }}
           transition={{
             y: {duration: 0.5},
             opacity: {duration: 3},
@@ -34,23 +51,24 @@ function Experience() {
           }}
           onAnimationComplete={() => setIsAnimating(false)}
         >
-          {!flipped[0] && !isAnimating[0]? 
+          {!flipped[2] ? 
             <div className='card-front'>
-              <p className='text-3xl font-bold'>Noveon Magentics</p>
+              <p className='text-3xl font-bold'>Upwork</p>
               <p className='text-2xl'>Full Stack Developer</p>
+              <p className='text-xs'>June 2021 - June 2022</p>
             </div>
-            :
+          :
             <div className='card-back'>
-              <li>Engineered robust RESTful APIs and microservices architecture, optimizing full-stack development workflow with Ruby on Rails and React for seamless data operations</li>
-              <li>Streamlined deployment processes through Docker containerization and Nginx configuration, ensuring consistent application performance across environments</li>
-              <li>Developed automated data processing solutions, integrating Python scripts for efficient Excel file parsing and database management</li>
+              <li>Modernized legacy Ruby on Rails applications through strategic gem updates and React-based UI enhancements, ensuring optimal system performance and user experience</li>
+              <li>Executed comprehensive framework updates while maintaining code integrity, focusing on security patches and performance optimization</li>
             </div>
           }
         </motion.div>
-        <motion.div className="bg-red-500 p-4 flip-card bg-cover rounded-lg cursor-pointer"
+        <motion.div className="p-4 flip-card bg-cover rounded-lg cursor-pointer"
           onClick={() => handleFlip(1)}
           initial = {{opacity: 0, y: '20vh'}}
-          animate = {{opacity: 1, y: 0, rotateY: flipped[1] ? 180: 0}}
+          whileInView = {{opacity: 1, y: 0, rotateY: flipped[1] ? 180: 0}}
+          viewport={{ once: true }}
           transition={{
             y: {duration: 1},
             opacity: {duration: 3},
@@ -62,6 +80,7 @@ function Experience() {
             <div className='card-front'>
               <p className='text-3xl font-bold'>Cognizant</p>
               <p className='text-2xl'>Data Engineer</p>
+              <p className='text-xs'>June 2021 - Oct 2022</p>
             </div>
           :
             <div className='card-back'>
@@ -72,10 +91,11 @@ function Experience() {
             </div>
           }
         </motion.div>
-        <motion.div className="bg-green-500 p-4 flip-card rounded-lg cursor-pointer"
-          onClick={() => handleFlip(2)}
+        <motion.div className= "p-4 flip-card rounded-lg cursor-pointer"
+          onClick={() => handleFlip(0)}
           initial = {{opacity: 0, y: '20vh'}}
-          animate = {{opacity: 1, y: 0, rotateY: flipped[2] ? 180: 0}}
+          whileInView = {{opacity: 1, y: 0, rotateY: flipped[0] ? 180: 0}}
+          viewport={{ once: true }} 
           transition={{
             y: {duration: 1.5},
             opacity: {duration: 3},
@@ -83,15 +103,17 @@ function Experience() {
           }}
           onAnimationComplete={() => setIsAnimating(false)}
         >
-          {!flipped[2] ? 
+          {!flipped[0] && !isAnimating[0]? 
             <div className='card-front'>
-              <p className='text-3xl font-bold'>Upwork</p>
+              <p className='text-3xl font-bold'>Noveon Magentics</p>
               <p className='text-2xl'>Full Stack Developer</p>
+              <p className='text-xs'>Oct 2022 - Present</p>
             </div>
-          :
+            :
             <div className='card-back'>
-              <li>Modernized legacy Ruby on Rails applications through strategic gem updates and React-based UI enhancements, ensuring optimal system performance and user experience</li>
-              <li>Executed comprehensive framework updates while maintaining code integrity, focusing on security patches and performance optimization</li>
+              <li>Engineered robust RESTful APIs and microservices architecture, optimizing full-stack development workflow with Ruby on Rails and React for seamless data operations</li>
+              <li>Streamlined deployment processes through Docker containerization and Nginx configuration, ensuring consistent application performance across environments</li>
+              <li>Developed automated data processing solutions, integrating Python scripts for efficient Excel file parsing and database management</li>
             </div>
           }
         </motion.div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 function Projects() {
   return (
     <>
-    <h2 className='text-4xl'>Projects</h2>
+    <h2 className='text-4xl project'>Projects</h2>
     <hr />
     </>
   )
